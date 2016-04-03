a1503331c051b50b9e59edf893fd4c1202f64676
Sireesh Gururaja <skg2123@columbia.edu>
2016-04-03 01:25:33 -0400

    getting first results from Watson STT

--- src/main/scala/com/gsireesh/App.scala
+++ src/main/scala/com/gsireesh/App.scala
@@ -1,14 +1,25 @@
 package com.gsireesh
 
+import com.ibm.watson.developer_cloud.speech_to_text.v1._
+import java.io.File
+
+import com.ibm.watson.developer_cloud.http.HttpMediaType
+
 /**
- * @author ${user.name}
+ * @author gsireesh
  */
 object App {
 
   def main(args : Array[String]) {
     val config = new Configuration("src/main/resources/config.properties")
 
+    val sttService = new SpeechToText()
+    sttService.setUsernameAndPassword(config("username"), config("password"))
 
+    val audio = new File("src/main/resources/speech_files/OSR_us_000_0010_8k.wav")
+    val options = new RecognizeOptions().model("en-US_NarrowbandModel").continuous(true).maxAlternatives(2)
+    val transcript = sttService.recognize(audio, HttpMediaType.AUDIO_WAV, options)
+    println(transcript)
   }
 
 }
