a7f78476c80a2dd7fc9b5832e5aec5e4fd0f87fa
Sireesh Gururaja <skg2123@columbia.edu>
2016-05-08 19:49:22 -0400

    Now printing colorized diffs using google diff-match-patch library

--- src/main/scala/com/gsireesh/App.scala
+++ src/main/scala/com/gsireesh/App.scala
@@ -2,6 +2,7 @@ package com.gsireesh
 
 import java.io.File
 
+import com.gsireesh.DiffWrapper.getPrintableDiff
 import com.gsireesh.SttServices.WatsonSttService
 
 /**
@@ -14,8 +15,16 @@ object App {
     val expectedValues = ExpectedValueLoader("src/main/resources/expected_values.csv")
     val watsonStt = new WatsonSttService(config("username"), config("password"))
 
-    val audio = new File("src/main/resources/speech_files/OSR_us_000_0010_8k.wav")
-    println(watsonStt.transcribe(audio))
+    for (key <- expectedValues.keys) {
+      val audio = new File("src/main/resources/speech_files/" + key)
+      //      val watsonResponses = watsonStt.transcribe(audio)
+      val watsonResponses = List("the birch canoes and then on the smooth planes", "glue the sheet to the dock background", "it is easy to tell the death of a well", "these days that you can they because they were dish", "rice is often served and rumbles", "the Jews of London's next", "the box was thrown beside the park truck", "the hogs are fed chopped corn and garbage", "four hours of study work faced us", "a large size and stockings is hard to sell")
+      val expectedResponses = expectedValues(key).map(x => x.toLowerCase.replaceAllLiterally("\"", ""))
+      val comparedResults = (expectedResponses, watsonResponses).zipped map ((a, b) => getPrintableDiff(a, b))
+      val output = new StringBuilder
+      comparedResults.addString(output, "\n\n")
+      println(output.toString())
+    }
   }
 
 }
---
+++ src/main/scala/com/gsireesh/DiffWrapper.scala
@@ -0,0 +1,30 @@
+package com.gsireesh
+
+import difflib.{DiffUtils, Patch}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.MutableList
+
+/**
+  * Created by sireesh on 5/8/16.
+  */
+object DiffWrapper {
+
+  def getPrintableDiff(expected: String, actual: String): String = {
+    val patch: Patch[String] = DiffUtils.diff(expected.split(' ').toSeq.asJava, actual.split(' ').toSeq.asJava)
+    var oldIndex = 0
+    val actualArray = actual.split(' ')
+    val actualArrayColored = new MutableList[String]
+    for (delta <- patch.getDeltas.asScala) {
+      actualArray.slice(oldIndex, delta.getRevised.getPosition).foreach(a => actualArrayColored += (Console.GREEN + a))
+      delta.getRevised.getLines.asScala.foreach(a => actualArrayColored += (Console.RED + a))
+      oldIndex = delta.getRevised.getPosition + delta.getRevised.getLines.size
+    }
+    if (oldIndex != actualArray.length) {
+      actualArray.drop(oldIndex).foreach(a => actualArrayColored += (Console.GREEN + a))
+    }
+    actualArrayColored += Console.RESET
+    expected + "\n" + actualArrayColored.mkString(" ")
+  }
+
+}
